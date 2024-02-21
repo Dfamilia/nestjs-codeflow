368129da0ee0c8ea47458c210ffd377e5d816433
dfamilia <dfamilia@intellisys.com.do>
2024-02-21 07:05:22 -0400

    adding a custom global exception filter

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -1,13 +1,42 @@
-import { ArgumentsHost, ExceptionFilter, Logger } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
 
 // this is a GLOBAL EXCEPTIONS IMPLEMENTATIONS that will help us to
 // to centrilize all exceptions events message like: BadRequest, Internal Errors, etc..
+@Catch()
 export class AllExceptionFilter implements ExceptionFilter {
   // Allows to log all the failure request events
   private readonly logger = new Logger(AllExceptionFilter.name);
 
-  //
+  // Allow us to manage our exceptons manages
   catch(exception: any, host: ArgumentsHost) {
-    throw new Error('Method not implemented.');
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    // validate if we recive the status of a fail http event or we'll return a default message if not
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const msg =
+      exception instanceof HttpException ? exception.getResponse() : exception;
+
+    this.logger.error(
+      `Http Status: ${status} Error Message: ${JSON.stringify(msg)}`,
+    );
+
+    response.status(status).json({
+      time: new Date().toISOString(),
+      path: request.url,
+      error: msg,
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // PERMITE USAR LAS VALIDACIONES DE "class-validator" GLOBALMENTE.
   app.useGlobalPipes(new ValidationPipe());
+  // PERMITE USAR LAS EXCEPTIONCES CUSTOMIZADAS QUE CREAMOS GLOABALMENTE.
+  app.useGlobalFilters(new AllExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
